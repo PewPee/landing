805ad6294a87532ad606a7ba1befea6e37a4b4bf
Daniel Taiakin <daniel@pewpee.com>
2023-02-14 03:09:26 +0300

    removed pewpee encored marquee

--- components/navigationbar.tsx
+++ components/navigationbar.tsx
@@ -9,11 +9,11 @@ import Marquee from 'react-fast-marquee'
 export default function NavigationBar() {
   return (
     <div>
-      <Marquee style={{height: "30px", background: "linear-gradient(to right, #ff00cc, #333399)"}} gradient={false}>
+      {/* <Marquee style={{height: "30px", background: "linear-gradient(to right, #ff00cc, #333399)"}} gradient={false}>
         {[...Array(10)].map((_, i) =>
           <span key={i} style={{marginRight: "100px"}}>#PewPeeEncored 2022 Coming Soon</span>
         )}
-      </Marquee>
+      </Marquee> */}
       <header className={barStyles.navWrapper}>
         <div className={`container ${barStyles.nav}`}>
           <Link href="/">
